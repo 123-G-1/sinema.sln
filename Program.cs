fd3140d2b3cc8d742f3f18f99f1b36f950390b1d
Gülbahar  DELİBAŞ <gulbahar123.1234@gmail.com>
2025-09-09 23:36:19 +0300

    sinema.sln
    C# ile geliştirilmiştir. Sinema Bilet Satışı

---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Salon s1 = new Salon();
+            Salon s2 = new Salon();
+            Salon s3 = new Salon();
+            s1.Kapasite = 50;
+            s2.Kapasite = 40;
+            s3.Kapasite = 30;
+            int secim1 = 0;
+            int secim2 = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 Salon 1 işlemleri");
+                Console.WriteLine("2 Salon 2 işlemleri");
+                Console.WriteLine("3 Salon 3 işlemleri");
+                Console.WriteLine("4 -Durum Görüntüle: ");
+                Console.WriteLine("5 -Çıkış");
+                Console.WriteLine();
+                Console.Write("İşlem seçiniz: ");
+                secim1 = Convert.ToInt32(Console.ReadLine());
+
+                if (secim1 == 1)
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Salon 1 işlemleri");
+                        Console.WriteLine("1 - Öğrenci Bileti Satışı");
+                        Console.WriteLine("2 - Tam Bilet Satışı");
+                        Console.WriteLine("3 - Tam Bilet İptali");
+                        Console.WriteLine("4 - Durum Görüntüle");
+                        Console.WriteLine("5 - Geri Dön");
+                        Console.WriteLine();
+                        Console.Write("İşlem seçiniz: ");
+                        secim2 = Convert.ToInt32(Console.ReadLine());
+
+
+                        if (secim2 == 1)
+                        {
+                            s1.ogrenciBiletSatısı();
+                            
+                           
+                        }
+                        else if (secim2 == 2)
+                        {
+                            s1.tamBiletSatısı();
+                          
+                        }
+                        else if (secim2 == 3)
+                        {
+                            s1.TamBiletIptali();
+                          
+                        }
+                        else if (secim2 == 4)
+                        {
+                            Console.Clear();
+                            s1.DurumGoruntule();
+                            Console.ReadLine();
+                        }
+                        else if (secim2 == 5)
+                        { 
+                            Console.WriteLine("Geri dönülüyor...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Geçersiz seçim");
+                            Console.ReadLine();
+                        }
+                    } while (secim2 != 5);
+
+                }
+
+                else if (secim1==2)
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Salon 2 işlemleri");
+                        Console.WriteLine("1 - Öğrenci Bileti Satışı");
+                        Console.WriteLine("2 - Tam Bilet Satışı");
+                        Console.WriteLine("3 - Bilet İptali");
+                        Console.WriteLine("4 - Durum Görüntüle");
+                        Console.WriteLine("5 - Geri Dön");
+                        Console.WriteLine();
+                        Console.Write("İşlem seçiniz: ");
+                        secim2 = Convert.ToInt32(Console.ReadLine());
+
+                        if (secim2 == 1)
+                        {
+                            s2.ogrenciBiletSatısı();
+                            Console.WriteLine("Öğrenci bileti satışı gerçekleşti");
+
+                        }
+                        else if (secim2 == 2)
+                        {
+                            s2.tamBiletSatısı();
+                            Console.WriteLine("Tam bilet satışı  gerçekleşti");
+
+                        }
+                        else if (secim2 == 3)
+                        {
+                            s2.TamBiletIptali();
+                            Console.WriteLine("Bilet iptali gerçekti");
+
+                        }
+                        else if (secim2 == 4)
+                        {
+                            Console.Clear();
+                            s2.DurumGoruntule();
+                            Console.ReadLine();
+                        }
+                        else if (secim2 == 5)
+                        {
+                            Console.WriteLine("Geri dönülüyor...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Geçersiz seçim");
+                            Console.ReadLine();
+                        }
+                    } while (secim2 != 5);
+
+                }
+                else if(secim1==3)
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Salon 3 işlemleri");
+                        Console.WriteLine("1 - Öğrenci Bileti Satışı");
+                        Console.WriteLine("2 - Tam Bilet Satışı");
+                        Console.WriteLine("3 - Bilet İptali");
+                        Console.WriteLine("4 - Durum Görüntüle");
+                        Console.WriteLine("5 - Geri Dön");
+                        Console.WriteLine();
+                        Console.Write("İşlem seçiniz: ");
+                        secim2 = Convert.ToInt32(Console.ReadLine());
+                        if (secim2 == 1)
+                        {
+                            s3.ogrenciBiletSatısı();
+                            Console.WriteLine("Öğrenci bileti satışı gerçekleşti");
+
+                        }
+                        else if (secim2 == 2)
+                        {
+                            s3.tamBiletSatısı();
+                            Console.WriteLine("Tam bilet satışı  gerçekleşti");
+
+                        }
+                        else if (secim2 == 3)
+                        {
+                            s3.TamBiletIptali();
+                            Console.WriteLine("Bilet iptali gerçekti");
+
+                        }
+                        else if (secim2 == 4)
+                        {
+                            Console.Clear();
+                            s3.DurumGoruntule();
+                            Console.ReadLine();
+                        }
+                        else if (secim2 == 5)
+                        {
+                            Console.WriteLine("Geri dönülüyor...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Geçersiz seçim");
+                            Console.ReadLine();
+                        }
+                    } while (secim2 != 5);
+                }
+                else if (secim1 == 4)
+                {
+                    s1.DurumGoruntule();
+                    s2.DurumGoruntule();
+                    s3.DurumGoruntule();
+
+                }
+                else if (secim1 == 5)
+                {
+                    Console.WriteLine("Çıkış yapılıyor...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz seçim, lütfen tekrar deneyin.");
+                }
+
+
+
+
+
+            }    while (secim1 != 5);
+        }
+
+    } 
+}
---
+++ Salon.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinema
+{
+    internal class Salon
+    {
+        int kapasite;
+        int ogrenciBiletSayisi = 0;
+        int tamBiletSayisi = 0;
+        int bakiye = 0;
+        //int bosKoltukSayisi;
+        private int adet;
+
+        public int Kapasite { get => kapasite; set => kapasite = value; }
+
+
+
+        public int Bakiye { get => bakiye; }
+        //public int BosKoltukSayisi { get => bosKoltukSayisi; }
+        public int OgrenciBiletSayisi { get => ogrenciBiletSayisi; }
+        public int TamBiletSayisi { get => tamBiletSayisi; }
+
+        public void tamBiletSatısı()
+        {
+            Console.Clear();
+            Console.Write("Kaç adet tam bilet satılacak");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            tamBiletSayisi += adet;
+            bakiye += adet * 50;
+            Console.WriteLine(adet + " adet Tam bilet satışı gerçekleşti.");
+            Console.ReadLine();
+
+        }
+
+        public void ogrenciBiletSatısı()
+        {
+            Console.Clear();
+            Console.Write("Kaç adet öğrenci bileti satılacak");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            ogrenciBiletSayisi += adet;
+            bakiye += adet * 30;
+            Console.WriteLine(adet + " adet öğrenci bileti satışı gerçekleşti.");
+            Console.ReadLine();
+        }
+        public void TamBiletIptali()
+        {
+
+
+            Console.Write("Kaç bilet iptal edilecek: ");
+            int iptalFiyati = Convert.ToInt32(Console.ReadLine());
+            if (adet >tamBiletSayisi)
+            {
+                Console.WriteLine("İptal edilecek bilet bulunmamaktadır.");
+
+            }
+            else
+            {
+                tamBiletSayisi -= adet
+                    ;
+                bakiye -= adet * 50;
+                Console.WriteLine(adet + " adet Tam bilet iptali gerçekleşti.");
+                Console.ReadLine();
+            }
+            
+        }
+        public void Ogrecibiletİptal()
+
+        {
+          Console.Write("Kaç bilet iptal edilecek: ");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            if (adet > ogrenciBiletSayisi)
+            {
+                Console.WriteLine("İptal edilecek bilet bulunmamaktadır.");
+            }
+            else
+            {
+                ogrenciBiletSayisi -= adet;
+                bakiye -= adet * 30;
+                Console.WriteLine(adet + " adet öğrenci bileti iptali gerçekleşti.");
+                Console.ReadLine();
+            }
+
+        }
+
+        public void DurumGoruntule()
+        {
+         Console.WriteLine();
+            Console.WriteLine(kapasite + " kapasite");
+            Console.WriteLine("Satılan Tam Bilet Sayısı: " + TamBiletSayisi);
+            Console.WriteLine("Satılan Öğrenci Bilet Sayısı: " + OgrenciBiletSayisi);
+            Console.WriteLine("Boş Koltuk Sayısı:"+(Kapasite - (OgrenciBiletSayisi + TamBiletSayisi)));
+            Console.WriteLine("Bakiye: " + Bakiye );
+            Console.WriteLine("........");
+            Console.WriteLine();
+
+
+        }
+
+
+
+
+    }
+}
